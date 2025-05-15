a3913e37615e4ea4f18e96b46153c776a237fd07
agbo sunday felicity <agbosony10@gmail.com>
2025-05-15 12:23:39 +0100

    remove root specifier from index.html

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2784076936"
+    serviceWorkerVersion: "498613759"
   }
 });
